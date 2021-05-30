2043565308f99995556be8f255e2bb9ec1eedb7d
ebuberoderick <ebuberoderick2@gmail.com>
2021-05-30 18:17:33 +0100

    installed

--- resources/js/Mixins/HttpClient.js
+++ resources/js/Mixins/HttpClient.js
@@ -2,11 +2,11 @@ import axios from 'axios';
 
 class HttpClient{
     constructor() {
-        // const   domain = process.env.MIX_API_DOMAIN, 
-        //         protocol = process.env.MIX_API_PROTOCOL,
+        const   domain = process.env.MIX_API_DOMAIN, 
+                protocol = process.env.MIX_API_PROTOCOL,
         // const   domain = 'cietest.com/api', demo.coingust.com
-        const   domain = 'demo.coingust.com/api',
-                protocol = 'https',
+        // const   domain = 'demo.coingust.com/api',
+        //         protocol = 'https',
                 appUri = `${protocol}://${domain}`;
 
         this.client = axios.create({
