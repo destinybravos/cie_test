d86bdbae6aaa1be9961df71e7440a84baf57f581
Zubisofts <zubitex40@gmail.com>
2021-06-01 15:38:56 +0100

    updated

--- resources/js/Components/StoryComponent.vue
+++ resources/js/Components/StoryComponent.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="">
-        <div v-if="show" @click="show=false" class="absolute transition-all duration-500 h-screen w-screen top-0 right-0 z-30" style="background:rgba(0,0,0,0.6)"></div>
+        <div v-if="show" @click="show=false" class="fixed transition-all duration-500 h-screen w-screen top-0 right-0 z-30" style="background:rgba(0,0,0,0.6)"></div>
         <div class="h-36 bg-white py-2 px-3 rounded-lg shadow">
             <h2 class="font-bold">
                 Stories
@@ -13,13 +13,13 @@
         </div>
         <div class="bg-white relative mt-3 rounded-md shadow-md">
             <div @click="show=true" class="w-full z-50 relative">
-                <div class="bg-white shadow-md cursor-pointer px-3 py-2">
+                <div class="bg-white shadow-md cursor-pointer px-3 py-2 relative">
                    <span class="w-full">
                         <img class="rounded-full h-8 shadow-lg inline-block align-top px-1 pt-2 bg-purple-200" :src="'/imgs/' + $page.user.profile_photo_path" :alt="$page.user.name" />
                         <span v-if="!show" class="text-xs text-gray-400 w-4/5">What's going on? #Hashing.. @Mention.. link..</span>
                         <textarea v-if="show" name="" id="" class="overflow-y-hidden resize-none" style="width:80%" placeholder="What's going on? #Hashing.. @Mention.. link.."></textarea>
                     </span>
-                    <span class="px-2 pt-2 right-4">
+                    <span class="px-2 pt-2 right-4 absolute">
                         <font-awesome-icon v-if="!show" :icon="['fas', 'camera']"/> 
                         <font-awesome-icon :icon="['far', 'newspaper']"/> 
                     </span>
@@ -30,6 +30,17 @@
             </div>
         </div>
             <div class="py-3">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus inventore temporibus blanditiis rem at repellat magnam harum nesciunt unde placeat reiciendis dicta, ab reprehenderit maiores consectetur veritatis totam cum consequatur!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus inventore temporibus blanditiis rem at repellat magnam harum nesciunt unde placeat reiciendis dicta, ab reprehenderit maiores consectetur veritatis totam cum consequatur!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus inventore temporibus blanditiis rem at repellat magnam harum nesciunt unde placeat reiciendis dicta, ab reprehenderit maiores consectetur veritatis totam cum consequatur!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus inventore temporibus blanditiis rem at repellat magnam harum nesciunt unde placeat reiciendis dicta, ab reprehenderit maiores consectetur veritatis totam cum consequatur!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus inventore temporibus blanditiis rem at repellat magnam harum nesciunt unde placeat reiciendis dicta, ab reprehenderit maiores consectetur veritatis totam cum consequatur!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus inventore temporibus blanditiis rem at repellat magnam harum nesciunt unde placeat reiciendis dicta, ab reprehenderit maiores consectetur veritatis totam cum consequatur!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus inventore temporibus blanditiis rem at repellat magnam harum nesciunt unde placeat reiciendis dicta, ab reprehenderit maiores consectetur veritatis totam cum consequatur!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus inventore temporibus blanditiis rem at repellat magnam harum nesciunt unde placeat reiciendis dicta, ab reprehenderit maiores consectetur veritatis totam cum consequatur!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus inventore temporibus blanditiis rem at repellat magnam harum nesciunt unde placeat reiciendis dicta, ab reprehenderit maiores consectetur veritatis totam cum consequatur!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus inventore temporibus blanditiis rem at repellat magnam harum nesciunt unde placeat reiciendis dicta, ab reprehenderit maiores consectetur veritatis totam cum consequatur!
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus inventore temporibus blanditiis rem at repellat magnam harum nesciunt unde placeat reiciendis dicta, ab reprehenderit maiores consectetur veritatis totam cum consequatur!
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus inventore temporibus blanditiis rem at repellat magnam harum nesciunt unde placeat reiciendis dicta, ab reprehenderit maiores consectetur veritatis totam cum consequatur!
             </div>
     </div>
