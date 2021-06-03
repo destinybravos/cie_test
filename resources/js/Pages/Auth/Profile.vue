69136cdfa829798d47474a59199ef62c63bc483e
noblefresh <okechinoble@gmail.com>
2021-06-03 21:27:29 +0800

    Work ongoing

--- resources/js/Pages/Auth/Profile.vue
+++ resources/js/Pages/Auth/Profile.vue
@@ -23,6 +23,88 @@
                             <div class="h-36 rounded shadow bg-white mx-auto">
 
                             </div>
+                            
+                            <!-- Search for Posts -->
+
+                            <div class="rounded py-3 px-2 my-2 shadow bg-white mx-auto">
+                                <label>Search for posts</label>
+                                <div class="border-2 relative bg-white shadow-sm w-full flex flex-row my-1 py-0 rounded-lg">
+                                    <input v-model="exp" required type="text" class="pr-3 focus:outline-none placeholder-gray-500 pl-3 rounded-lg w-full bg-gray-50 py-2" placeholder="Years of Experience">
+                                </div>
+                            </div>
+
+                            <!-- Info  -->
+                            <div class="rounded py-3 my-2 shadow bg-white mx-auto">
+                                <div class="first-sec border-b border-gray-500 py-3 px-3">
+                                    <span class="py-1 px-1 bg-red-800 rounded-full text-xs mr-1">
+                                        <font-awesome-icon :icon="['fas', 'info-circle']" class="text-white" />
+                                    </span>
+                                    <span class="font-bold">Info</span>
+                                </div>
+                                <div class="middle-sec py-2 px-3">
+                                    <span class="py-1 px-1 text-xs mr-1">
+                                        <font-awesome-icon :icon="['fas', 'eye']" class="text-gray-700" />
+                                    </span>
+                                    <span class="text-green-400 text-xs">Online</span>
+                                </div>
+                                <div class="middle-sec py-1 mx-3 border-b border-gray-500">
+                                    <span class="py-1 px-1 text-xs mr-1">
+                                        <font-awesome-icon :icon="['fas', 'book']" class="text-gray-700" />
+                                    </span>
+                                    <span class="text-gray-500 text-xs">1 posts</span>
+                                </div>
+                                <div class="middle-sec py-1 px-3">
+                                    <span class="px-1 text-xs mr-1">
+                                        <font-awesome-icon :icon="['far', 'user']" class="text-gray-700" />
+                                    </span>
+                                    <span class="text-gray-500 text-sm">Male</span>
+                                </div>
+                            </div>
+
+                            <!-- Items -->
+                            <div class="rounded py-3 my-2 shadow bg-white mx-auto">
+                                <div class="first-sec px-3 text-sm">
+                                    <span class="py-1 px-1 bg-red-800 rounded-full text-xs mr-1">
+                                        <font-awesome-icon :icon="['far', 'images']" class="text-white" />
+                                    </span>
+                                    <span class="font-bold">Albums</span> 0
+                                </div>
+                            </div>
+
+                            <div class="rounded py-3 my-2 shadow bg-white mx-auto">
+                                <div class="first-sec px-3 text-sm">
+                                    <span class="py-1 px-1 bg-red-800 rounded-full text-xs mr-1">
+                                        <font-awesome-icon :icon="['fas', 'user-plus']" class="text-white" />
+                                    </span>
+                                    <span class="font-bold">Following</span> 0
+                                </div>
+                            </div>
+
+                            <div class="rounded py-3 my-2 shadow bg-white mx-auto">
+                                <div class="first-sec px-3 text-sm">
+                                    <span class="py-1 px-1 bg-red-800 rounded-full text-xs mr-1">
+                                        <font-awesome-icon :icon="['fas', 'users']" class="text-white" />
+                                    </span>
+                                    <span class="font-bold">Followers</span> 0
+                                </div>
+                            </div>
+
+                            <div class="rounded py-3 my-2 shadow bg-white mx-auto">
+                                <div class="first-sec px-3 text-sm">
+                                    <span class="py-1 px-1 bg-red-800 rounded-full text-xs mr-1">
+                                        <font-awesome-icon :icon="['fas', 'thumbs-up']" class="text-white" />
+                                    </span>
+                                    <span class="font-bold">Like</span> 0
+                                </div>
+                            </div>
+                            <div class="rounded py-3 my-2 shadow bg-white mx-auto">
+                                <div class="first-sec px-3 text-sm">
+                                    <span class="py-1 px-1 bg-red-800 rounded-full text-xs mr-1">
+                                        <font-awesome-icon :icon="['fas', 'users']" class="text-white" />
+                                    </span>
+                                    <span class="font-bold">Groups</span> 0
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -38,6 +120,7 @@
     import NavList from '../../Components/NavList.vue'
     import Cover from '../../Components/Cover.vue'
     import ShareFeed from '../../Components/ShareFeed.vue'
+import Label from '../../Jetstream/Label.vue'
 
     export default {
         components: {
@@ -47,6 +130,7 @@
             NavList, 
             Cover,
             ShareFeed,        
+                Label
         },
         data(){
             return {
